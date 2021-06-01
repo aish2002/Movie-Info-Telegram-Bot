9df68e7262cbde3abcc8cdbcce0f52ca90ac52ab
Aishwarya A J <56962523+aish2002@users.noreply.github.com>
2021-06-01 17:34:17 +0530

    Added port

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ import ssl
 import itertools
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import decouple
-
+import os
+PORT = int(os.environ.get('PORT', 5000))
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
